aedf5efa2d9c572405f22aed5b4b91da9cb546a3
lnoueryo <popo62520908@gmail.com>
2021-04-15 22:06:52 +0900

    ナビとページを分離

--- components/Navi.vue
+++ components/Navi.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div :style="ready?{opacity:1, transition: 'all 1s'}:{opacity:0, transition: 'all 1s'}">
         <el-row class="tac" style="width: 200px;position:fixed;z-index:2;height:100%" :style="transform">
             <el-col :span="12" style="height:100%">
                 <el-menu style="width:200px;height:100%;" default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
@@ -54,32 +54,47 @@
             <el-menu-item index="3">Info</el-menu-item>
             <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">Orders</a></el-menu-item>
         </el-menu> -->
-        <div style="width:100%;margin:auto;position:absolute;z-index:3;padding: 10px;">
-        <!-- <div style="width:100%;margin:auto;background-color:white;position:absolute;z-index:3;padding: 10px;background: linear-gradient(#DA397080, 80%, #DA397080);"> -->
-            <div style="max-width:850px;width:100%;margin:auto;padding:0 6%;display:flex;align-items:center">
-                <img style="position:relative;z-index:2;width:100%;max-width:40px;filter: brightness(0.7) hue-rotate(15deg) contrast(120%)" :src="require('~/assets/images/logo_transparent.png')" alt="">
-                <img style="position:relative;z-index:2;width:100%;max-width:200px;filter: brightness(0.7) hue-rotate(50deg) contrast(120%)" :src="require('~/assets/images/letter_transparent.png')" alt="">
-            </div>
-        </div>
-        <div style="position:relative;">
-            <div style="padding:80px 6% 30px 6%;position:relative;z-index:1;max-width:850px;margin:auto">
-                <div style="padding:3% 50px;margin-bottom:2%">
-                    <img :src="require('~/assets/images/logo/start-programming6.png')" alt="" style="filter: drop-shadow(2px 4px 6px black);max-width:350px;margin-bottom:3%">
+        <div ref="main" style="height:100%;position:relative" :style="{minHeight: innerHeight}">
+            <div style="position:relative;max-width: 1200px;margin:auto;">
+                <div style="width: 100%;margin: auto;position: relative;">
+                    <div style="width:100%;z-index:3;padding: 20px;display:flex;align-items:center;justify-content:flex-start;flex-wrap:wrap;padding:0 6%">
+                        <div style="padding:20px 0;display:flex;align-items:center">
+                            <router-link :to="{name: 'home'}">
+                                <img class="pc-only" style="position:relative;z-index:2;width:100%;max-width:60px;filter: brightness(6) hue-rotate(15deg) contrast(120%)" :src="require('~/assets/images/logo_transparent.png')" alt="">
+                                <img style="position:relative;z-index:2;width:100%;max-width:300px;filter: brightness(6) hue-rotate(50deg) contrast(120%)" :src="require('~/assets/images/letter_transparent.png')" alt="">
+                            </router-link>
+                        </div>
+                        <div style="display:flex;color:white;font-size:16px;width: 100%;max-width: 690px;justify-content:space-around;z-index:1">
+                            <div @click="scroll($refs.section1.getBoundingClientRect().top)">当サイトの目的</div>
+                            <div @click="scroll($refs.section2.getBoundingClientRect().top)">はじめよう</div>
+                            <div @click="scroll($refs.section2.getBoundingClientRect().top)">技術ブログ</div>
+                        <router-link :to="{name: 'components'}">UIコンポーネント</router-link>
+                        </div>
+                    </div>
                 </div>
-                <el-carousel :interval="6000" type="card" height="200px">
-                    <el-carousel-item v-for="item in items" :key="item">
-                        <h3 class="medium" style="height:100%"><div style="justify-content:center;align-items:center;display:flex;height:100%"><img :src="item" alt="" style="max-height:120px;display:flex;align-items:center"></div></h3>
-                    </el-carousel-item>
-                </el-carousel>
+                <div style="">
+                    <div id="second-image-container">
+                        <img :src="require('~/assets/images/logo/start-programming6.png')" alt="" style="filter: drop-shadow(2px 4px 6px black);max-width:550px;width:100%;">
+                    </div>
+                    <!-- <el-carousel :interval="6000" type="card" height="200px">
+                        <el-carousel-item v-for="item in items" :key="item">
+                            <h3 class="medium" style="height:100%"><div style="justify-content:center;align-items:center;display:flex;height:100%"><img :src="item" alt="" style="max-height:120px;display:flex;align-items:center"></div></h3>
+                        </el-carousel-item>
+                    </el-carousel> -->
+                </div>
+                <carousel :sources="videos" delimiter="true" arrow="true" @ready="ready=true" video="true"></carousel>
             </div>
-            <div :style="{backgroundImage: `url(${require('~/assets/images/mv4.jpg')})`}" style="filter: hue-rotate(300deg) contrast(1.5);background-position: center;position:absolute;top:0;bottom:0;right:0;left:0;"></div>
+            <div :style="{backgroundImage: `url(${require('~/assets/images/mv4.jpg')})`}" style="background-size: cover;filter: hue-rotate(300deg) contrast(1.5);background-position: center;position:absolute;top:0;bottom:0;right:0;left:0;"></div>
         </div>
-        <div class="VueToNuxtLogo"><div class="Triangle Triangle--two" /><div class="Triangle Triangle--one" /><div class="Triangle Triangle--three" /><div class="Triangle Triangle--four" /></div>
     </div>
 </template>
 
 <script>
+import Carousel from '../components/Carousel'
 export default {
+    components:{
+        Carousel
+    },
     data() {
         return {
             activeIndex: '1',
@@ -127,25 +142,46 @@ export default {
                 require('~/assets/images/logo/ubuntu.png'),
                 require('~/assets/images/logo/ios.png'),
                 require('~/assets/images/logo/android.png'),
-            ]
+            ],
+            position: 0,
+            videos: [
+                'https://www.youtube.com/embed/f3HHw7ycYrc',
+                'https://www.youtube.com/embed/aW3mWnfRuZQ',
+                'https://www.youtube.com/embed/A0ecp_JNRKw',
+                'https://www.youtube.com/embed/jWIEQxZLWw4',
+                'https://www.youtube.com/embed/PppZnHGaChI',
+                'https://www.youtube.com/embed/tWGwxuZGQYE',
+                'https://www.youtube.com/embed/6I8dnrB4WMI'
+            ],
+            ready: false,
         }
     },
-  computed:{
-    transform(){
-      return this.drawer ? {transform: 'translateX(0px)', transition: 'all .3s'} : {transform: 'translateX(-200px)', transition: 'all .3s'};
-    }
-  },
-  methods:{
-    handleSelect(key, keyPath) {
-      console.log(key, keyPath);
+    computed:{
+        transform(){
+            return this.drawer ? {transform: 'translateX(0px)', transition: 'all .3s'} : {transform: 'translateX(-200px)', transition: 'all .3s'};
+        },
+        innerHeight(){
+            return this.$store.getters.innerHeight + 'px';
+        },
     },
-    handleOpen(key, keyPath) {
-    console.log(key, keyPath);
-    },
-    handleClose(key, keyPath) {
-    console.log(key, keyPath);
+    methods:{
+        scroll(to, h){
+            let i = h | 0;
+            let that = this;
+            if (i < to) {
+                setTimeout(() => {
+                window.scrollTo(0, i);
+                that.scroll(to, i + 200);
+                }, 1);
+            }
+        },
+        handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+        },
+        handleClose(key, keyPath) {
+        console.log(key, keyPath);
+        }
     }
-  }
 }
 </script>
 
@@ -165,7 +201,21 @@ export default {
   .el-carousel__item:nth-child(2n+1) {
     background-color: #d3dce6;
   }
-
+  #second-image-container{
+    text-align:center;
+    padding:25px 6% 0px 6%;
+    z-index:1;
+    margin:auto;
+    position:relative;
+  }
+    @media screen and (max-width: 480px){
+        .pc-only{
+            display: none
+        }
+        #second-image-container{
+            padding-bottom:30px;
+        }
+    }
 
 
 </style>
--- pages/home.vue
+++ pages/home.vue
@@ -4,5 +4,24 @@
         <!-- <img :src="require(`~/assets/images/mv3.jpg`)" style="filter: hue-rotate(300deg);height: 300px;object-fit: cover;object-position: top;width: 100%;"> -->
         <!-- <img :src="require(`~/assets/images/mv4.jpg`)" style="filter: hue-rotate(300deg);height: 300px;object-fit: cover;object-position: top;width: 100%;"> -->
         <!-- <img :src="require(`~/assets/images/mv5.jpg`)" style="filter: hue-rotate(300deg);height: 300px;object-fit: cover;object-position: top;width: 100%;"> -->
+        <main>
+            <div id="main-wrapper">
+                <div id="main-container" style="padding:80px 20px 80px 20px;position:relative;z-index:1;max-width:1100px;margin:30px auto;background-color:#E779A2;">
+                    <div style="padding:80px 6% 80px 6%;background-color:white;box-shadow:inset 0 -3em 3em rgba(0,0,0,0.1), 0 0 0 2px rgb(255,255,255), 0.3em 0.3em 1em rgba(0,0,0,0.3);">
+                        <section>
+                            <div ref="section1" style="transform: translateY(-2%)"></div>
+                            <div>
+                                <h1>当サイトの目的</h1>
+                            </div>
+                        </section>
+                        <section ref="section2">
+                            <div>
+                                <h1>はじめよう</h1>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </main>
     </div>
 </template>
